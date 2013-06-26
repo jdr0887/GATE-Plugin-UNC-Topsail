0a427d2d24f8860825ae0232d056272229dd5b6c
Jason Reilly <jdr0887@renci.org>
2013-06-26 10:45:34 -0400

    posterity commit

--- unc-topsail-service/src/test/java/org/renci/gate/plugin/topsail/Scratch.java
+++ unc-topsail-service/src/test/java/org/renci/gate/plugin/topsail/Scratch.java
@@ -1,10 +1,7 @@
 package org.renci.gate.plugin.topsail;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
@@ -15,17 +12,12 @@ import org.renci.gate.GlideinMetric;
 import org.renci.jlrm.Queue;
 import org.renci.jlrm.Site;
 import org.renci.jlrm.slurm.SLURMJobStatusInfo;
-import org.renci.jlrm.slurm.ssh.SLURMSSHJob;
 import org.renci.jlrm.slurm.ssh.SLURMSSHLookupStatusCallable;
 
 public class Scratch {
 
-    
-    
-    
     @Test
     public void testLookupMetrics() {
-        List<SLURMSSHJob> jobCache = new ArrayList<SLURMSSHJob>();
 
         Map<String, GlideinMetric> metricsMap = new HashMap<String, GlideinMetric>();
 
@@ -36,7 +28,9 @@ public class Scratch {
         site.setSubmitHost("topsail-sn.unc.edu");
         site.setMaxTotalPending(4);
         site.setMaxTotalRunning(4);
+
         Map<String, Queue> queueInfoMap = new HashMap<String, Queue>();
+
         Queue queue = new Queue();
         queue.setMaxJobLimit(10);
         queue.setMaxMultipleJobsToSubmit(2);
@@ -49,7 +43,7 @@ public class Scratch {
 
         try {
 
-            SLURMSSHLookupStatusCallable callable = new SLURMSSHLookupStatusCallable(site, jobCache);
+            SLURMSSHLookupStatusCallable callable = new SLURMSSHLookupStatusCallable(site);
             Set<SLURMJobStatusInfo> jobStatusSet = Executors.newSingleThreadExecutor().submit(callable).get();
 
             // get unique list of queues
@@ -58,14 +52,7 @@ public class Scratch {
                 for (SLURMJobStatusInfo info : jobStatusSet) {
                     queueSet.add(info.getQueue());
                 }
-                for (SLURMSSHJob job : jobCache) {
-                    queueSet.add(job.getQueueName());
-                }
-            }
 
-            Set<String> alreadyTalliedJobIdSet = new HashSet<String>();
-
-            if (jobStatusSet != null && jobStatusSet.size() > 0) {
                 for (SLURMJobStatusInfo info : jobStatusSet) {
                     if (metricsMap.containsKey(info.getQueue())) {
                         continue;
@@ -74,7 +61,6 @@ public class Scratch {
                         continue;
                     }
                     metricsMap.put(info.getQueue(), new GlideinMetric(0, 0, info.getQueue()));
-                    alreadyTalliedJobIdSet.add(info.getJobId());
                 }
 
                 for (SLURMJobStatusInfo info : jobStatusSet) {
@@ -94,35 +80,6 @@ public class Scratch {
                 }
             }
 
-            Iterator<SLURMSSHJob> jobCacheIter = jobCache.iterator();
-            while (jobCacheIter.hasNext()) {
-                SLURMSSHJob nextJob = jobCacheIter.next();
-                for (SLURMJobStatusInfo info : jobStatusSet) {
-
-                    if (!nextJob.getName().equals(info.getJobName())) {
-                        continue;
-                    }
-
-                    if (!alreadyTalliedJobIdSet.contains(nextJob.getId()) && nextJob.getId().equals(info.getJobId())) {
-                        switch (info.getType()) {
-                            case PENDING:
-                                metricsMap.get(info.getQueue()).incrementPending();
-                                break;
-                            case RUNNING:
-                                metricsMap.get(info.getQueue()).incrementRunning();
-                                break;
-                            case CANCELLED:
-                            case COMPLETED:
-                            case FAILED:
-                            case TIMEOUT:
-                                jobCacheIter.remove();
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-            }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
@@ -131,7 +88,7 @@ public class Scratch {
             GlideinMetric metric = metricsMap.get(key);
             System.out.println(metric.toString());
         }
-        
+
     }
 
 }
